92925fcb3ef2f0f6c818b30d80ff51e74e8b9cfb
KOSASIH <kosasihg88@gmail.com>
2024-11-05 10:05:29 +0700

    Create requestLogger.js

---
+++ src/middleware/requestLogger.js
@@ -0,0 +1,10 @@
+// src/middleware/requestLogger.js
+
+const requestLogger = (req, res, next) => {
+    const { method, url } = req;
+    const timestamp = new Date().toISOString();
+    console.log(`[${timestamp}] ${method} ${url}`);
+    next();
+};
+
+module.exports = requestLogger;
