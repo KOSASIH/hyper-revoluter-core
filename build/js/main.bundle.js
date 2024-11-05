6447a6c07233b93c41d667c3efc7844d8571f011
KOSASIH <kosasihg88@gmail.com>
2024-11-05 16:48:54 +0700

    Update main.bundle.js

--- build/js/main.bundle.js
+++ build/js/main.bundle.js
@@ -1,1 +1,14 @@
+// main.bundle.js - Example of compiled JavaScript
+(function() {
+    'use strict';
 
+    // Your application code here
+    console.log('Application is running!');
+
+    // Example function
+    function greetUser(name) {
+        console.log(`Hello, ${name}!`);
+    }
+
+    greetUser('World');
+})();
